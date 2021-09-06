905fcf9d70ae67e6318b7fa92bf453d086a68a1a
Bence Borbás <benceborbas88@gmail.com>
2021-09-06 13:18:23 +0200

    Task 01

--- angular/src/app/components/student-form/student-form.component.ts
+++ angular/src/app/components/student-form/student-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Student } from 'src/app/models/student';
+import { StudentHttpService } from 'src/app/service/student-http.service';
+import { ClassroomComponent } from '../classroom/classroom.component';
 
 @Component({
   selector: 'app-student-form',
@@ -11,14 +15,37 @@ export class StudentFormComponent implements OnInit {
   @ViewChild('form') form: NgForm;
   reactForm: FormGroup;
 
+  studentID: string;
+  studentForm: Student = {firstName:'', lastName:'', email:'', classroom: ClassroomComponent}
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private studentService: StudentHttpService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.studentID = this.route.snapshot.params.id;
+    if(this.studentID){
+      this.studentService.getById(this.studentID).subscribe(
+        data => {
+          this.studentForm = data;
+        }
+      )
+   }
   }
 
-  saveStudent(){
-
+  saveStudent(form: NgForm){
+    if(this.studentID){
+      this.studentService.update(form.value, this.studentID).subscribe(
+        student => this.router.navigate(['student-list']),
+        err => console.error(err)
+      )
+    }else {
+      this.studentService.save(form.value).subscribe(
+        student => this.router.navigate(['student-list']),
+        err => console.error(err)
+      )
+    }
   }
-
 }
